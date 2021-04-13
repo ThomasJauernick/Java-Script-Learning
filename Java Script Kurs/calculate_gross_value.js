18ab88cbee1b9c0052e5dfb1e06c01bf8a189eb8
ThomasJauernick <45935503+ThomasJauernick@users.noreply.github.com>
2021-04-13 17:39:18 +0200

    Add files via upload

--- Java Script Kurs/calculate_gross_value.js
+++ Java Script Kurs/calculate_gross_value.js
@@ -1,6 +1,12 @@
-var NetValue = prompt ("Please input your netprice here")
-var Tax = 0.19
-var TaxPercent = (Tax*100)
-var GrossValue = (NetValue*(1+Tax))
-var TaxValue = (NetValue*Tax)
-alert("With a Net Value of "+NetValue+"EUR and a Tax Value of "+TaxPercent+"% will have a Tax value of "+TaxValue+"EUR and a Gross Value of "+GrossValue+"EUR")
+var UserInput = prompt ("Please input your netvalue here")
+
+CalculateGrossValue(UserInput);
+
+function CalculateGrossValue (NetValue) {
+var NetValue;
+var Tax = 0.19;
+var TaxPercent = (Tax*100);
+var GrossValue = (NetValue*(1+Tax));
+var TaxValue = (NetValue*Tax);
+alert("With a Net Value of "+NetValue+"EUR and a Tax Value of "+TaxPercent+"% will have a Tax value of "+TaxValue+"EUR and a Gross Value of "+GrossValue+"EUR");
+}
